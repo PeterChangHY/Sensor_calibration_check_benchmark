2405634111667be05a1d44d7cb201689d4eef1bf
changhuanyang <changhuanyang@gmail.com>
2021-08-17 18:39:08 -0700

    Add lidar and corner radar into dumper

--- scripts/dumper.py
+++ scripts/dumper.py
@@ -28,6 +28,7 @@ import json
 
 import utils
 import re
+import pypcd
 
 bridge = CvBridge()
 
@@ -90,6 +91,7 @@ def writeOdomMsg(odom_msg, file_path):
 def writeRadarMsg(radar_msg, file_path):
     radar_message = radar_msg.message
     radar_list = []
+    pointcloud_data_list= []
     for i in range(len(radar_message.tracks)):
         radar_track = {
             "timestamp": radar_message.header.stamp.to_sec(),
@@ -106,17 +108,35 @@ def writeRadarMsg(radar_msg, file_path):
             ],
             "track_shape": [(p.x, p.y, p.z) for p in radar_message.tracks[i].track_shape.points],
         }
-    
         radar_list.append(radar_track)
 
     with open(file_path, "w") as o:
         o.write(json.dumps(radar_list))
 
-def getTopics(all_topics):
+
+def writeRadarMsgPCD(radar_msg, file_path):
+    radar_message = radar_msg.message
+    radar_list = []
+    pointcloud_data_list= []
+    for i in range(len(radar_message.tracks)):
+        for p in radar_message.tracks[i].track_shape.points:
+            pointcloud_data_list.append([p.x, p.y, p.z])
+
+    # convet it to pcd
+    pointcloud_data = np.array(pointcloud_data_list)
+    pypcd.pypcd.save_point_cloud_bin(
+        pypcd.make_xyz_point_cloud(pointcloud_data), file_path
+    )
+
+def writeLidarMsg(lidar_msg, file_path):
+    pc = pypcd.PointCloud.from_msg(lidar_msg.message)
+    pc.save(file_path)
+
+def getTopics(extraced_topics):
     """get the necessary topics
 
     Args:
-        all_topics:
+        extraced_topics:
             all topics existed in the bag
     Return:
         topics:
@@ -134,35 +154,56 @@ def getTopics(all_topics):
         return None
 
     topics = {}
-    front_left_topic = search_pattern(["/front_left_camera/image_(color|raw)/compressed"], all_topics)
+    front_left_topic = search_pattern(["/front_left_camera/image_(color|raw)/compressed"], extraced_topics)
     if front_left_topic is None:
         print("Can't find front_left_camera topic")
         return None
     topics['front_left_camera'] = front_left_topic
     
-    front_right_topic = search_pattern(["/front_right_camera/image_(color|raw)/compressed"], all_topics)
+    front_right_topic = search_pattern(["/front_right_camera/image_(color|raw)/compressed"], extraced_topics)
     if front_right_topic is None:
         print("Can't find front_right_camera topic")
         return None
     topics['front_right_camera'] = front_right_topic
     
-    odom_topic = search_pattern(["/navsat/odom"], all_topics)
+    odom_topic = search_pattern(["/navsat/odom"], extraced_topics)
     if front_right_topic is None:
         print("Can't find odom topic")
         return None
     topics['odom'] = odom_topic
     
-    front_radar_topic = search_pattern(["(^/conti_bumper_radar/conti_bumper_radar/radar_tracks$|^/conti_bumper_radar/radar_tracks$|^/bumper_radar/radar_tracks$|^/front_center_radar/radar_tracks$|^/front_bumper_radar/radar_tracks$)"], all_topics)
+    front_radar_topic = search_pattern(["(^/conti_bumper_radar/conti_bumper_radar/radar_tracks$|^/conti_bumper_radar/radar_tracks$|^/bumper_radar/radar_tracks$|^/front_center_radar/radar_tracks$|^/front_bumper_radar/radar_tracks$)"], extraced_topics)
     if front_radar_topic is None:
         print("Can't find front_radar topic")
         return None
     topics['front_radar'] = front_radar_topic
 
+    left_corner_radar_topic = search_pattern(["^/left_corner_radar/radar_tracks"], extraced_topics)
+    if left_corner_radar_topic is not None:
+        topics['left_corner_radar'] = left_corner_radar_topic
+    else :
+        print("Can't find left_corner_radar_topic")
+    right_corner_radar_topic = search_pattern(["^/right_corner_radar/radar_tracks"], extraced_topics)
+    if right_corner_radar_topic is not None:
+        topics['right_corner_radar'] = right_corner_radar_topic
+    else :
+        print("Can't find right_corner_radar_topic")
+    
+    left_lidar_topic = search_pattern(["^/os1_left/points"], extraced_topics)
+    if left_lidar_topic is not None:
+        topics['left_lidar'] = left_lidar_topic
+    else :
+        print("Can't find left_lidar_topic")
+    right_lidar_topic = search_pattern(["^/os1_right/points"], extraced_topics)
+    if right_lidar_topic is not None:
+        topics['right_lidar'] = right_lidar_topic
+    else :
+        print("Can't find right_lidar_topic")
     return topics
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("datadumper")
-    parser.add_argument("--bags", type=str, nargs='?',
+    parser.add_argument("--bags", type=str, nargs='*',
                         help="bag_paths")
     parser.add_argument("-d", "--datalist",
                         help="yaml file containing bags to process, if exist it will add into bags",
@@ -199,33 +240,57 @@ if __name__ == "__main__":
         bags = []
 
     # add bags in yaml files
-    data_list = utils.parse_yaml_file(args.data_list, "foo_ws", "foo_build")
-    for data in data_list:
-        bags.append(data.bag_file)
+    if args.data_list:
+        data_list = utils.parse_yaml_file(args.data_list, "foo_ws", "foo_build")
+        for data in data_list:
+            bags.append(data.bag_file)
 
     for bag in bags:
         bag_instance = load_rosbag_from_file(bag)
-        topics = getTopics(bag_instance.get_type_and_topic_info().topics.keys())
-        if topics is None:
+        bag_all_topics = getTopics(bag_instance.get_type_and_topic_info().topics.keys())
+        if bag_all_topics is None:
             print(bag)
             print(bag_instance.get_type_and_topic_info().topics.keys())
             print('fail')
             continue
 
-        front_left_topic = topics['front_left_camera']
-        front_right_topic = topics['front_right_camera']
-        front_radar_topic = topics['front_radar']
-        odom_topic = topics['odom']
+        front_left_topic = bag_all_topics['front_left_camera']
+        front_right_topic = bag_all_topics['front_right_camera']
+        front_radar_topic = bag_all_topics['front_radar']
+        odom_topic = bag_all_topics['odom']
 
-        all_topics = [front_left_topic, front_right_topic, front_radar_topic, odom_topic]
 
+        extraced_topics = [front_left_topic, front_right_topic, front_radar_topic, odom_topic]
+
+        # optional topic will extrac if exist
+        left_lidar_topic = "foo"
+        if "left_lidar" in bag_all_topics:
+            left_lidar_topic = bag_all_topics["left_lidar"]
+            extraced_topics.append(left_lidar_topic)
+        
+        right_lidar_topic = "foo"
+        if "right_lidar" in bag_all_topics:
+            right_lidar_topic = bag_all_topics["right_lidar"]
+            extraced_topics.append(right_lidar_topic)
+        
+        left_corner_radar_topic = "foo"
+        if "left_corner_radar" in bag_all_topics:
+            left_corner_radar_topic = bag_all_topics["left_corner_radar"]
+            extraced_topics.append(left_corner_radar_topic)
+        
+        right_corner_radar_topic = "foo"
+        if "right_corner_radar" in bag_all_topics:
+            right_corner_radar_topic = bag_all_topics["right_corner_radar"]
+            extraced_topics.append(right_corner_radar_topic)
+        
         out_subdir = os.path.join(outdir, os.path.basename(bag))
 
         # prepare output directories for each sensor
-        front_left_image_dir = os.path.join(out_subdir, front_left_topic.split("/")[1])
-        front_right_image_dir = os.path.join(out_subdir, front_right_topic.split("/")[1])
-        front_radar_dir = os.path.join(out_subdir, front_radar_topic.split("/")[1])
-        odom_dir = os.path.join(out_subdir, odom_topic.split("/")[1])
+        sub_dirs = {}
+        # print(extraced_topics)
+        for topic in extraced_topics:
+            # print(topic)
+            sub_dirs[topic] =  os.path.join(out_subdir, topic.split("/")[1])
 
         count = 0
 
@@ -259,38 +324,67 @@ if __name__ == "__main__":
             radar_filename = os.path.join(dir, "{:04d}.json".format(count))
             temp_file_dict[radar_filename] = tempfile.mkstemp(suffix='.json')[1]
             writeRadarMsg(radar_msg, temp_file_dict[radar_filename])
+            writeRadarPCD2Temp(radar_msg, temp_file_dict, count, dir)
+            return True
+        
+        def writeRadarPCD2Temp(radar_msg, temp_file_dict, count, dir):
+            radar_filename = os.path.join(dir, "{:04d}.pcd".format(count))
+            temp_file_dict[radar_filename] = tempfile.mkstemp(suffix='.pcd')[1]
+            writeRadarMsgPCD(radar_msg, temp_file_dict[radar_filename])
+            return True
+        
+        def writeLidar2Temp(lidar_msg, temp_file_dict, count, dir):
+            lidar_filename = os.path.join(dir, "{:04d}.pcd".format(count))
+            temp_file_dict[lidar_filename] = tempfile.mkstemp(suffix='.pcd')[1]
+            writeLidarMsg(lidar_msg,temp_file_dict[lidar_filename])
             return True
 
 
-        for frame in buffered_message_generator(bags=[bag_instance], topics=all_topics, tolerance=args.tolerance, rate=args.rate):
+
+        for frame in buffered_message_generator(bags=[bag_instance], topics=extraced_topics, tolerance=args.tolerance, rate=args.rate):
 
             if front_left_topic not in frame:
                 continue
             front_left_img_message = frame[front_left_topic]
-            if writeImg2Temp(front_left_img_message, temp_file_dict, count, front_left_image_dir) is None:
+            if writeImg2Temp(front_left_img_message, temp_file_dict, count, sub_dirs[front_left_topic]) is None:
                 continue
 
             if front_right_topic not in frame:
                 continue
             front_right_img_message = frame[front_right_topic]
-            if writeImg2Temp(front_right_img_message, temp_file_dict, count, front_right_image_dir) is None:
+            if writeImg2Temp(front_right_img_message, temp_file_dict, count, sub_dirs[front_right_topic]) is None:
                 continue
 
             if front_radar_topic not in frame:
                 continue
             front_radar_message = frame[front_radar_topic]
-            if writeRadar2Temp(front_radar_message, temp_file_dict, count, front_radar_dir) is None:
+            if writeRadar2Temp(front_radar_message, temp_file_dict, count, sub_dirs[front_radar_topic]) is None:
                 continue
+            
+            if left_lidar_topic in frame:
+                if not writeLidar2Temp( frame[left_lidar_topic], temp_file_dict, count, sub_dirs[left_lidar_topic]):
+                    print("failed to write left_lidar")
+            if right_lidar_topic in frame:
+                if not writeLidar2Temp( frame[right_lidar_topic], temp_file_dict, count, sub_dirs[right_lidar_topic]):
+                    print("failed to write right_lidar")
+            
+            if left_corner_radar_topic in frame:
+                writeRadar2Temp(  frame[left_corner_radar_topic], temp_file_dict, count, sub_dirs[left_corner_radar_topic])
+
+            if right_corner_radar_topic in frame:
+                writeRadar2Temp(frame[right_corner_radar_topic], temp_file_dict, count, sub_dirs[right_corner_radar_topic])
+
+
 
             if odom_topic not in frame:
                 continue
             odom_message = frame[odom_topic]
-            if writeOdom2Temp(odom_message, temp_file_dict, count, odom_dir) is None:
+            if writeOdom2Temp(odom_message, temp_file_dict, count, sub_dirs[odom_topic]) is None:
                 continue
 
             # write stereo_problem
-            front_left_relpath = os.path.relpath(os.path.join(front_left_image_dir, "{:04d}.jpg".format(count)), out_subdir)
-            front_right_relpath = os.path.relpath(os.path.join(front_right_image_dir, "{:04d}.jpg".format(count)), out_subdir)
+            front_left_relpath = os.path.relpath(os.path.join(sub_dirs[front_left_topic], "{:04d}.jpg".format(count)), out_subdir)
+            front_right_relpath = os.path.relpath(os.path.join(sub_dirs[front_right_topic], "{:04d}.jpg".format(count)), out_subdir)
             problem_text = " ".join([front_left_relpath, front_right_relpath])
             stereo_problem_f.write(problem_text + '\n')
             count = count + 1
@@ -301,10 +395,8 @@ if __name__ == "__main__":
 
         if count >= args.max_number_of_frames:
             file_utils.safe_make_dir(out_subdir)
-            file_utils.safe_make_dir(front_left_image_dir)
-            file_utils.safe_make_dir(front_right_image_dir)
-            file_utils.safe_make_dir(front_radar_dir)
-            file_utils.safe_make_dir(odom_dir)
+            for topic in extraced_topics:
+                file_utils.safe_make_dir(sub_dirs[topic])
             for key in temp_file_dict:
                 shutil.move(temp_file_dict[key], key)
 
